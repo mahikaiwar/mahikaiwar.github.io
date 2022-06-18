d23c1daae4dbc3d6dbcb392c1fc852e1708b66b8
Chirag Ravindra <cravindra@gmail.com>
2022-06-18 17:15:01 +0530

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    Hi, I'm Mahi
+  </header>
+  <main>
+    Main
+  </main>
+  <footer>
+    Footer
+  </footer>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+$background: rgb(249,179,179,0.71);
+
+html, body {
+  margin: 0;
+  padding: 0;
+  background: $background;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: whitesmoke;
+
+}
+
+header {
+  height: 100vh;
+  width: 100vw;
+  font-size: 8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
