bb24e39f8d38db121a2a48a13c2d7a29a3291821
mahikaiwar <107754291+mahikaiwar@users.noreply.github.com>
2022-06-18 18:54:50 +0530

    Added TNWA images

--- src/config.js
+++ src/config.js
@@ -5,7 +5,43 @@ export const projects = [
         description: "This project was for a literature course on performance, where we had to make a creative adaptation of Shakespeare's 'Twelfth Night' in any medium. As a collaboration with a friend, this project explores the idea of love and the self in a modern webtoon adaptation of Act II Scene IV. Here are a few snippets from the project. Here is the link to the whole project (it is designed to be read from a smartphone) : https://www.webtoons.com/en/challenge/twelfth-night-act-ii-scene-iv/list?title_no=762533",
         heroImage: "tnwacover.jpg",
         images: [{
-            img: 'img.png',
+            img: 'tnwaopen.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwagae.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwasetting.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwaolivia.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwasimp.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwaviola.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwaresign.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwaoops.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwapass.jpg',
+            title: '',
+            description: ''
+        },{
+            img: 'tnwapinkies.jpg',
             title: '',
             description: ''
         }]
