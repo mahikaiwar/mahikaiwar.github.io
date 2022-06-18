e376a2a22a960d012446a022f24c57c13ef5f9ca
mahikaiwar <107754291+mahikaiwar@users.noreply.github.com>
2022-06-18 18:48:11 +0530

    Added TNWA description

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         id: "tnwa",
         title: "Twelfth Night Webtoon Adaptation",
-        description: "",
+        description: "This project was for a literature course on performance, where we had to make a creative adaptation of Shakespeare's 'Twelfth Night' in any medium. As a collaboration with a friend, this project explores the idea of love and the self in a modern webtoon adaptation of Act II Scene IV. Here are a few snippets from the project. Here is the link to the whole project (it is designed to be read from a smartphone) : https://www.webtoons.com/en/challenge/twelfth-night-act-ii-scene-iv/list?title_no=762533",
         heroImage: "tnwacover.jpg",
         images: [{
             img: 'img.png',
