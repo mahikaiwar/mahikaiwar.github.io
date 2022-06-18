4fa70d57506e65cb9440fa1c218e399db69c1ab6
Chirag Ravindra <cravindra@gmail.com>
2022-06-18 18:07:47 +0530

    set up prject structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <header>
-    Hi, I'm Mahi
+    <div class="content">
+      <p class="title">
+        Hi, I'm Mahi
+      </p>
+      <div class="cta">
+        <a class="btn" href="#work">Nice to meet you!</a>
+      </div>
+    </div>
   </header>
   <main>
-    Main
+    <section id="work">
+      <h1>My Work</h1>
+      <div class="gallery">
+
+      </div>
+    </section>
   </main>
   <footer>
     Footer
@@ -15,19 +27,27 @@
 
 export default {
   name: 'App',
-  components: {
-  }
-}
+  components: {}
+};
 </script>
 
 <style lang="scss">
 
+@mixin buttonBG($bg) {
+  border: 2px solid $bg;
+  color: $bg;
+  &:hover {
+    background-color: $bg;
+    transition: all 0.3s ease-in-out;
+  }
+}
 
-$background: rgb(249,179,179,0.71);
+$background: rgb(249, 179, 179, 0.71);
 
 html, body {
   margin: 0;
   padding: 0;
+  border: 0;
   background: $background;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -36,12 +56,62 @@ html, body {
 
 }
 
+.btn {
+  @include buttonBG(#fff);
+  width: 180px;
+  height: 80px;
+  outline: none;
+  cursor: pointer;
+  background: none;
+  transition: .5s;
+  font-size: 24px;
+  border-radius: 5px;
+  font-family: "Lato", sans-serif;
+  text-decoration: none;
+  padding: 20px 30px;
+
+  &:hover {
+    border: none;
+    color: #823535;
+    font-size: 28px;
+    transform: scale(1.1);
+  }
+}
+
 header {
-  height: 100vh;
-  width: 100vw;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  height: calc(100vh - 10px);
   font-size: 8rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background: url('@/assets/Banner.png');
+  background-position: center;
+  background-size: cover;
+
+  .content {
+    width: 100%;
+    height: 100%;
+    background: rgb(57 42 40 / 60%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    p{
+      margin: 1rem 0;
+    }
+    .btn {
+      width: 400px;
+    }
+  }
+}
+
+main {
+  section {
+    height: 98vh;
+    h1 {
+      text-align: center;
+      font-size: 8rem;
+    }
+  }
 }
 </style>
---
+++ src/config.js
@@ -0,0 +1,13 @@
+export projects = [
+    {
+        id: "tnwa",
+        title: "Twelfth Night Webtoon Adaptation",
+        description: "",
+        heroImage: "img.png",
+        images: [{
+            img: 'img.png',
+            title: '',
+            description: ''
+        }]
+    }
+]
